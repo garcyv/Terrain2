2e8e08d94660a55b037ae5687ad3ce19ae9f0bf1
garcyv <garcyv@gmail.com>
2021-03-06 15:02:28 -0300

    second fragment

--- prueba/app/src/main/java/com/example/prueba/SecondFragment.kt
+++ prueba/app/src/main/java/com/example/prueba/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.terrain
+package com.example.prueba
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,39 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
-import com.example.terrain.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
-    private lateinit var binding: FragmentSecondBinding
-    private val viewModel: MarsViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding= FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel.selectedMarsTerrain.observe(viewLifecycleOwner, Observer {
-        it?.let {
-            Glide.with(binding.imvSecond).load(it.imgSrc).into(binding.imvSecond)
-           binding.tvSecond2.text = it.type
-           binding.tvSecond3.text = it.price.toString()
-
-            }
-        })
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
